88bcf9deea6b264efee1ae1790ca2f76ce599c9c
rra.roro@gmail.com <rra.roro@gmail.com>
2019-01-11 17:30:09 +0300

    Add "helloworld" program from Home Work 1

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+﻿#include "lib.h"
+
+#include <iostream>
+
+using namespace std;
+// #include <spdlog/spdlog.h>
+
+int main(int argc, char *argv[])
+{
+      //    auto logger = spdlog::stdout_logger_mt("console");
+      //    logger->info("version {} was started", version());
+
+      cout << "Hello world!!\n";
+      cout << "PROJECT_VERSION_PATCH: " << version() << "\n";
+
+      return 0;
+}
