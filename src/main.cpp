b9a2c813fa1df43f52f45aa28909cce1a56aece9
rra.roro@gmail.com <rra.roro@gmail.com>
2019-01-12 13:19:34 +0300

    Remove "spdlog" lib

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,11 @@
 #include <iostream>
 
 using namespace std;
-// #include <spdlog/spdlog.h>
 
 int main(int argc, char *argv[])
 {
-      //    auto logger = spdlog::stdout_logger_mt("console");
-      //    logger->info("version {} was started", version());
+      (void)argc;
+      (void)argv;
 
       cout << "Hello world!!\n";
       cout << "PROJECT_VERSION_PATCH: " << version() << "\n";
